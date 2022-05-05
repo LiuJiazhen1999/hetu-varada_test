5856e76226932e31af03ba9166896a0549c5b7ae
Liujiazhen1999 <1779927845@qq.com>
2022-05-05 13:10:56 +0800

    l

---
+++ trino_query_parquet.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Basename;
+use File::Path;
+use List::Util;
+
+# GLOBALS
+my $SCRIPT_NAME = basename( __FILE__ );
+my $SCRIPT_PATH = dirname( __FILE__ );
+
+open(TRINO_PARQUET_QUERY_LOG, ">trino_parquet_query.log") or die "can not open trino_parquet_query.log";
+
+chdir $SCRIPT_PATH;
+chdir 'tpcds_parquet_sql';
+my @queries = glob '*.sql';
+for my $query ( @queries ) {
+
+	print "parquet query : $query start \n";
+	my $queryStart = time();
+	my $cmd="(./trino --server localhost:8080 --catalog hive -f ./$query)";
+	my @warnoutput=`$cmd`;
+
+	my $queryEnd = time();
+	my $queryTime = $queryEnd - $queryStart;
+	print "parquet Query : $query In $queryTime secs\n";
+	print TRINO_PARQUET_QUERY_LOG "parquet $query : $queryTime\n";
+}
