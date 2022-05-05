bab81b075a7ad8bb04565c1641f3b3b8bad10af1
Liujiazhen1999 <1779927845@qq.com>
2022-05-05 13:21:01 +0800

    l

--- trino_query.pl
+++ trino_query.pl
@@ -20,7 +20,7 @@ for my $query ( @queries ) {
 
 	print "parquet query : $query start \n";
 	my $queryStart = time();
-	my $cmd="(./trino --server localhost:8080 --catalog hive -f ./$query)";
+	my $cmd="(/home/ec2-user/bigdata/trino-server-370/bin/trino --server localhost:8080 --catalog hive -f ./$query)";
 	my @warnoutput=`$cmd`;
 
 	my $queryEnd = time();
@@ -35,7 +35,7 @@ for my $query ( @queries ) {
 
         print "orc query : $query start \n";
         my $queryStart = time();
-        my $cmd="(./trino --server localhost:8080 --catalog hive -f ./$query)";
+        my $cmd="(/home/ec2-user/bigdata/trino-server-370/bin/trino --server localhost:8080 --catalog hive -f ./$query)";
         my @warnoutput=`$cmd`;
 
         my $queryEnd = time();
