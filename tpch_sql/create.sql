418daba008645f479cf5238ecd35e50d76c6dfbb
Liujiazhen1999 <1779927845@qq.com>
2022-05-22 10:26:07 +0800

    liu

--- tpch_sql/create.sql
+++ tpch_sql/create.sql
@@ -2,3 +2,4 @@ create schema if not exists tpch_parquet_1000;
 use hive.tpch_parquet_1000;
 CREATE TABLE hive.tpch_parquet_1000.lineitem WITH (format = 'parquet') AS SELECT * FROM tpch.sf1000.lineitem;
 CREATE TABLE hive.tpch_parquet_1000.lineitemsub WITH (format = 'parquet') AS SELECT * FROM hive.tpch_parquet_1000.lineitem where shipdate <= DATE '1998-12-01' - INTERVAL '90' DAY;
+CREATE TABLE hive.tpch_parquet_1000.lineitemsub14 WITH (format = 'parquet') AS SELECT * FROM hive.tpch_parquet_1000.lineitemsub where shipdate >= DATE '1995-09-01' AND shipdate < DATE '1995-09-01' + INTERVAL '1' MONTH;
