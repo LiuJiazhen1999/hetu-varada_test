2a81853ae95f691f7545ba644ee3964d90a3fb87
Liujiazhen1999 <1779927845@qq.com>
2022-05-25 15:20:52 +0800

    liu

--- python_script/scatter.py
+++ python_script/scatter.py
@@ -78,7 +78,9 @@ if __name__ == "__main__":
         _files = os.listdir(_path)
         _files = [_path + _file for _file in _files]
         for _file in _files:
-            _column = _file.split("_")[-1]
+            #_column = _file.split("_")[-1]
+            _temp =  _file.split("-")[-1]
+            _column = _temp[_temp.index("_") + 1 : -1]
             print(_file)
             print(_column)
             print(_table)
