c6c9c9fe1c1c670ffcc4898c87e8f64d0d059e6f
Liujiazhen1999 <1779927845@qq.com>
2022-06-16 19:24:56 +0800

    liu

--- python_script/filterScatter.py
+++ python_script/filterScatter.py
@@ -4,6 +4,7 @@ import pyarrow.parquet as pp
 import matplotlib.pyplot as plt
 import numpy as np
 
+from jaccardCompute import com_jaccard
 from indexConstruct import get_sys_min_max_value, get_min_max_value
 
 def is_lt(column_type, value1, value2):
@@ -31,6 +32,7 @@ def is_eq(column_type, value1, value2):
         return float(value1) == float(value2)
 
 def draw_scatter(file, dot_size, column, column_type, _start, _end):
+    _jaccard = com_jaccard(file, column, column_type)
     _table = pp.ParquetFile(file)
     num_of_row_groups = _table.num_row_groups
     x1 = []#(x1, y1)单个row group的(_min, _max)，浅色
@@ -42,6 +44,9 @@ def draw_scatter(file, dot_size, column, column_type, _start, _end):
         _min, _max = get_sys_min_max_value(column_type)
         _flag = False
         for _ in row_group_contents.column(column):
+            print(str(_))
+            if str(_) == 'None':
+                continue
             _min = get_min_max_value(column_type, str(_), _min)[0]
             _max = get_min_max_value(column_type, str(_), _max)[1]
             if _start == _end:
@@ -65,31 +70,21 @@ def draw_scatter(file, dot_size, column, column_type, _start, _end):
         #print(str(row_group_index) + "-" + str(_min) + "-" + str(_max))
     fig = plt.figure()
     ax1 = fig.add_subplot(1, 1, 1)
-    ax1.set_title("Fit percent:" + str(len(x2)/(len(x1) + len(x2))))
+    ax1.set_title("Fit percent:" + str(len(x2)/(len(x1) + len(x2))) + " jaccard:" + str(_jaccard))
     ax1.set_xlabel('min-value')
     ax1.set_ylabel('max-value')
     ax1.scatter(x1, y1, s=dot_size, c='lightgrey', marker='.')
     ax1.scatter(x2, y2, s=dot_size, c='k', marker='.')
     plt.savefig(file + column + "_" + _start + "-" + _end + ".png")
+    #plt.show()
     plt.close()
 
 if __name__ == "__main__":
-    file_name = "20220524_062839_00026_fs53m_66cba92c-a213-4e48-ab15-5b37a5bb0daa"
-    draw_scatter(
-        "/mydata/tpch_parquet_300.db_rewrite/orders/" + file_name,
-        10, "orderdate", "date",
-        str("1993-1-1"),
-        str("1993-2-1"))
-
-    # file_name = "20220523_132919_00005_fs53m_ff916c89-b7ca-4b23-b0f1-efd34a8ec887"
-    # draw_scatter(
-    #     "/mydata/tpcds_parquet_300.db_rewrite/catalog_sales/" + file_name,
-    #     10, "cs_net_profit", "float",
-    #     str("0"),
-    #     str("500"))
-    #
-    # draw_scatter(
-    #     "/mydata/tpcds_parquet_300.db_rewrite/catalog_sales/" + file_name,
-    #     10, "cs_net_paid_inc_ship", "float",
-    #     str("10000"),
-    #     str("10500"))
+    file_name = "20220523_132505_00004_fs53m_509e9f99-1d58-4cf7-9d90-98845500e399"
+    draw_scatter("/mydata/tpcds_parquet_300.db_rewrite/catalog_returns/" + file_name, 10, "cr_refunded_customer_sk", "int", str("174822"), str("174822"))
+    draw_scatter("/mydata/tpcds_parquet_300.db_rewrite/catalog_returns/" + file_name, 10, "cr_refunded_cdemo_sk", "int", str("174822"), str("174822"))
+    draw_scatter("/mydata/tpcds_parquet_300.db_rewrite/catalog_returns/" + file_name, 10, "cr_returning_customer_sk", "int", str("174822"), str("174822"))
+    draw_scatter("/mydata/tpcds_parquet_300.db_rewrite/catalog_returns/" + file_name, 10, "cr_returning_addr_sk", "int", str("174822"), str("174822"))
+    file_name = "20220523_142254_00019_fs53m_0a18d3c4-b84b-4ebe-b1e7-317a8391df3f"
+    draw_scatter("/mydata/tpcds_parquet_300.db_rewrite/store_sales/" + file_name, 10, "ss_customer_sk", "int", str("174822"), str("174822"))
+    draw_scatter("/mydata/tpcds_parquet_300.db_rewrite/store_sales/" + file_name, 10, "ss_cdemo_sk", "int", str("174822"), str("174822"))
